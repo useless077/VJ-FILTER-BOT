a06ab970dbaf80a9f967dcfffb2259dc0b0b24e2
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-04-23 15:37:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,6 +107,10 @@ LANGUAGES = ["malayalam", "mal", "tamil", "tam" ,"english", "eng", "hindi", "hin
 SEASONS = ["season 1", "season 2", "season 3", "season 4", "season 5", "season 6", "season 7", "season 8", "season 9", "season 10"]
 EPISODES = ["E01", "E02", "E03", "E04", "E05", "E06", "E07", "E08", "E09", "E10", "E11", "E12", "E13", "E14", "E15", "E16", "E17", "E18", "E19", "E20", "E21", "E22", "E23", "E24", "E25", "E26", "E27", "E28", "E29", "E30", "E31", "E32", "E33", "E34", "E35", "E36", "E37", "E38", "E39", "E40"]
 QUALITIES = ["360p", "480p", "720p", "1080p", "1440p", "2160p"]
+EMOJIS = [
+        "👍", "❤‍🔥", "❤", "🔥", 
+        "🥰", "👻", "😁", "🤔"
+]
 
 
                            # Don't Remove Credit @VJ_Botz
