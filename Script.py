6f3952dee9f9dd49a478b492a263a10230955c29
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-04-23 15:52:53 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -229,13 +229,8 @@ IMDb Data:
 
 <b>🏷 Title</b>: <a href={url}>{title}</a>
 🎭 Genres: {genres}
-📆 Year: <a href={url}/releaseinfo>{year}</a>
 🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
-☀️ Languages : <code>{languages}</code>
-📀 RunTime: {runtime} Minutes
 📆 Release Info : {release_date}
-🎛 Countries : <code>{countries}</code>
-
 
 ⏰Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
 
