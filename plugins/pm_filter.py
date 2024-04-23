89de0065ad08afe5c4716c5aa76129bb36269b2d
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-04-23 15:39:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -39,6 +39,7 @@ from database.gfilters_mdb import (
 import logging
 from urllib.parse import quote_plus
 from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size
+from random import choice
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
