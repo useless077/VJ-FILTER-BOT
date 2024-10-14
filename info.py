3a6da9ecffc45571de6f96aabab961cf1ad7ea49
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-10-14 14:51:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -144,7 +144,7 @@ else:
 URL = environ.get("URL", "https://townbus-tamilbots.koyeb.app/")
 
 # redirect to channel for files 
-REDIRECT_TO = (environ.get('REDIRECT_TO', channel))
+REDIRECT_TO = (environ.get('REDIRECT_TO', true))
 
 
 # Rename Info ; If True Then Bot Rename File Else Not
