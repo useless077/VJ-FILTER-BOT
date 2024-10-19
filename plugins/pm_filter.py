a543da7e391db0ff3996b43f1c8acec8778e216c
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-10-19 08:03:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1526,7 +1526,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 f'<b> Hai 👋 {query.from_user.mention} </b>😍\n\n<b>📫 Your File is Ready</b>\n\n<b>📂 Fɪʟᴇ Nᴀᴍᴇ</b> : <code>[TownBus] {title}</code>\n\n<b>⚙️ Fɪʟᴇ Sɪᴢᴇ</b> : <b>{size}</b>',
         #        quote=True,
         #        disable_web_page_preview=True,
-                parse_mode=enums.ParseMode.HTML,
+        #        parse_mode=enums.ParseMode.HTML,
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
@@ -1580,9 +1580,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
             )
             mssg1 = await query.answer(
                 f'<b> Hai 👋 {query.from_user.mention} </b>😍\n\n<b>📫 Your File is Ready</b>\n\n<b>📂 Fɪʟᴇ Nᴀᴍᴇ</b> : <code>[TownBus] {title}</code>\n\n<b>⚙️ Fɪʟᴇ Sɪᴢᴇ</b> : <b>{size}</b>',
-                quote=True,
+        #        quote=True,
          #       disable_web_page_preview=True,
-                parse_mode=enums.ParseMode.HTML,
+          #      parse_mode=enums.ParseMode.HTML,
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
