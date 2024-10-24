0c6dba18b3e9c4a4ed78a06467da22c64389c6fc
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-10-24 17:55:23 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -64,7 +64,6 @@ class Database:
         'vjtech': None,
         'vj_tech': None
     }
-    
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
         self.db = self._client[database_name]
