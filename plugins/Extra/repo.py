c42dac189e74e2683606a676afc05ec482281fd5
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-11-12 13:49:41 +0530

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -35,7 +35,7 @@ async def git(bot, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powered by :</b> @VJ_Botz
+<b>Powered by :</b> @TamilBots
 
 """
         if qw.get("description"):
