cc4e9615cf4e6f09afcbcbe5047c1683f5d3532a
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-11-12 13:20:09 +0530

    Update engine.py

--- plugins/Extra/engine.py
+++ plugins/Extra/engine.py
@@ -3,7 +3,7 @@
 # Ask Doubt on telegram @KingVJ01
 
 import openai
-
+import os
 async def ai(query):
     openai.api_key =  os.environ.get["OPENAI_API_KEY"] #Your openai api key
     response = openai.Completion.create(engine="text-davinci-002", prompt=query, max_tokens=100, n=1, stop=None, temperature=0.9, timeout=5)
