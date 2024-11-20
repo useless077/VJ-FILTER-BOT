5575bfcf52ca45c27fd54ca182264b14b8c19a6b
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-11-20 12:37:40 +0530

    Update sample.py

--- plugins/sample.py
+++ plugins/sample.py
@@ -1,3 +1,3 @@
-# Don't Remove Credit @VJ_Botz 
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ 
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @TamilBots
+# Subscribe YouTube Channel For Amazing Bot @Tamilbots
+# Ask Doubt on telegram @TamilSupport
