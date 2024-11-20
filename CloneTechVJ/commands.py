8e3dad6cbc02f0759e09f4e1deca31801146442d
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-11-20 16:51:26 +0530

    Update commands.py

--- CloneTechVJ/commands.py
+++ CloneTechVJ/commands.py
@@ -49,7 +49,7 @@ async def start(client, message):
             up = cd["update_channel_link"]
             buttons.append([InlineKeyboardButton('🍿 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ 🍿', url=up)])
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgUAAxkBAAEKVaxlCWGs1Ri6ti45xliLiUeweCnu4AACBAADwSQxMYnlHW4Ls8gQMAQ") 
+        m=await message.reply_sticker("CAACAgUAAx0CfU1WbQACA39nPa4Y9iV6TDxDm1imQF4wQtRvpgACdQMAApd4IVWZtUc0cRhVsB4E") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_text(
