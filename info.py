09fb6790a9eae9699f3a13bb6c4da357c85447fa
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-11-20 20:03:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,7 +52,7 @@ ADMIN_GROUP_ID = int(environ.get('ADMIN_GROUP_ID', '-1002102220397'))
 
 #Redirect to Channel
 #Must change this link to work redirect (FILE_FORWORD)
-FILE_FORWARD = environ.get('FILE_FORWARD',"https://t.me/tamil5k")
+FILE_FORWARD = environ.get('FILE_FORWARD',"tamil5k")
 FILE_CHANNEL = int(environ.get('FILE_CHANNEL', 0))
 
 # MongoDB information
@@ -71,7 +71,7 @@ PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟ�
 OWNER_USERNAME = environ.get('OWNER_USERNAME', 'YaarulaNee') # owner username without @
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://Tamilvc:Tamilvc@cluster0.euced.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Necessary If clone mode is true
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'vendru') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
