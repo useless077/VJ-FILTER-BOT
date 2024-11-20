87f6945581da84507cc8b327d9d0539f03bcdca7
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-11-20 12:25:00 +0530

    Update lyrics.py

--- plugins/Extra/lyrics.py
+++ plugins/Extra/lyrics.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @TamilBots
+# Subscribe YouTube Channel For Amazing Bot @Tamilbots
+# Ask Doubt on telegram @TamilSupport
 
 
 from pyrogram import Client, filters
