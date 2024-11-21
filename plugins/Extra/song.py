6d3ddea3671b399c81a3a1b05b9fe5e48d7f1bc1
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2024-11-21 08:48:14 +0530

    Update song.py

--- plugins/Extra/song.py
+++ plugins/Extra/song.py
@@ -76,7 +76,7 @@ def a(client, message):
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-        xy = await.message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name)
+        xy = await message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name)
         await m.delete()
         await asyncio.sleep(300)
         await xy.delete()
